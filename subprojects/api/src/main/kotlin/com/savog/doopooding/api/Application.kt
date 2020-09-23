471f4ebaedf320bf97e4216c75d05464f0d0d4de
chohyeonkeun <>
2020-09-24 02:17:57 +0900

    Application 빈 설정 및 gradle 변경

--- subprojects/api/src/main/kotlin/com/savog/doopooding/api/Application.kt
+++ subprojects/api/src/main/kotlin/com/savog/doopooding/api/Application.kt
@@ -1,13 +1,18 @@
 package com.savog.doopooding.api
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.module.SimpleModule
+import com.google.protobuf.Message
 import com.savog.doopooding.client.login.SnsClientProperties
 import com.savog.doopooding.core.properties.AwsConfigurationProperties
 import com.savog.doopooding.core.properties.S3ConfigurationProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.http.MediaType
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
@@ -29,6 +34,18 @@ class SavogApiServer : WebMvcConfigurer {
     override fun configureContentNegotiation(configurer: ContentNegotiationConfigurer) {
         // Accept 헤더가 따로 주어지지 않았을 때 기본 응답을 JSON으로 한다.
         configurer.defaultContentType(MediaType.APPLICATION_JSON)
+    }
+
+    @Bean
+    fun objectMapper(): ObjectMapper {
+        return ObjectMapper()
+    }
+
+    @Bean
+    fun mappingJackson2HttpMessageConverter(objectMapper: ObjectMapper): MappingJackson2HttpMessageConverter {
+        return MappingJackson2HttpMessageConverter().apply {
+            this.objectMapper = objectMapper
+        }
     }
 }
 
