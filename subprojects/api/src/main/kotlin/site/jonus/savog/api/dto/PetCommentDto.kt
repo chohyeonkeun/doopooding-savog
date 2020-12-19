5b51f0e13f41bd3c6a3f2c2af9029d696d0fa1dd
jonus <>
2020-12-19 20:07:04 +0900

    pet comment/disease/treatmentHistory 각 DTO 생성하여 pet detail service에 추가

--- subprojects/api/src/main/kotlin/site/jonus/savog/api/dto/PetCommentDto.kt
+++ subprojects/api/src/main/kotlin/site/jonus/savog/api/dto/PetCommentDto.kt
@@ -2,6 +2,10 @@ package site.jonus.savog.api.dto
 
 data class PetCommentDto(
     val total: Int,
+    val comments: List<PetCommentInfo>
+)
+
+data class PetCommentInfo(
     val commentId: Long? = null,
     val petId: Long? = null,
     val userId: Long? = null,
